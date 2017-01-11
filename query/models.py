8e64d605ec6db481bff296a6e057bd4598bd4648
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-01-11 13:03:20 +0800

    add chinese package model

--- query/models.py
+++ query/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 class BlackcatPackage(models.Model):
-    query_id = models.CharField(max_length=100, primary_key=True)
+    blakcat_id = models.CharField(max_length=100, primary_key=True)
     state = models.CharField(max_length=255)
     login_date = models.DateTimeField('login date')
     establishment = models.CharField(max_length=255)
 
     def __str__(self):
-        return '{} at {}'.format(self.state, self.login_date)
+        return '{}: {} at {}'.format(
+            self.blakcat_id, self.state, self.login_date
+        )
+
+class ChinesePackage(models.Model):
+    chinese_id = models.CharField(max_length=100, primary_key=True)
+    state = models.CharField(max_length=255)
+    login_date = models.DateTimeField('login date')
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}: {} at {}'.format(
+            self.chinese_id, self.state, self.login_date
+        )
