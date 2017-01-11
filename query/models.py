f5fbfc1848a3bf7b5693bcdeea6f43754a52f3d7
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-01-11 15:02:35 +0800

    fix typo

--- query/models.py
+++ query/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class BlackcatPackage(models.Model):
-    blakcat_id = models.CharField(max_length=100, primary_key=True)
+    blackcat_id = models.CharField(max_length=100, primary_key=True)
     state = models.CharField(max_length=255)
     login_date = models.DateTimeField('login date')
     establishment = models.CharField(max_length=255)
 
     def __str__(self):
         return '{}: {} at {}'.format(
-            self.blakcat_id, self.state, self.login_date
+            self.blackcat_id, self.state, self.login_date
         )
 
 class ChinesePackage(models.Model):
