514397dc131ffb2e112c02103e5b485ff01199a7
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-02-08 10:49:33 +0800

    columns nullable and default is null

--- query/models.py
+++ query/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class BlackcatPackage(models.Model):
     blackcat_id = models.CharField(max_length=100, primary_key=True)
-    state = models.CharField(max_length=255)
-    login_date = models.DateTimeField('login date')
-    establishment = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, null=True, blank=True, default=None)
+    login_date = models.DateTimeField('login date', null=True, blank=True, default=None)
+    establishment = models.CharField(max_length=255, null=True, blank=True, default=None)
 
     def __str__(self):
         return '{}: {} at {}'.format(
@@ -13,9 +13,9 @@ class BlackcatPackage(models.Model):
 
 class ChinesePackage(models.Model):
     chinese_id = models.CharField(max_length=100, primary_key=True)
-    state = models.CharField(max_length=255)
-    login_date = models.DateTimeField('login date')
-    location = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, null=True, blank=True, default=None)
+    login_date = models.DateTimeField('login date', null=True, blank=True, default=None)
+    location = models.CharField(max_length=255, null=True, blank=True, default=None)
 
     def __str__(self):
         return '{}: {} at {}'.format(
