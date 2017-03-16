80d47cccc57b32f956bf499428d864ebb7f7c665
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-03-16 13:31:17 +0800

    follow js standard

--- query/static/js/package.js
+++ query/static/js/package.js
@@ -1,68 +1,68 @@
 $(function() {
-  $("#add-new-form").on("submit", function(event) {
-    event.preventDefault();
-    createNewId();
-  });
+  $('#add-new-form').on('submit', function (event) {
+    event.preventDefault()
+    createNewId()
+  })
 
-  function createNewId() {
-    var package_info = {
-        query_id: $("#queryIdInput").val(),
-        blackcat_id: $("#blackcatIdInput").val(),
-        chinese_id: $("#chineseIdInput").val()
+  function createNewId () {
+    var packageInfo = {
+        query_id: $('#queryIdInput').val(),
+        blackcat_id: $('#blackcatIdInput').val(),
+        chinese_id: $('#chineseIdInput').val()
     }
     $.ajax({
-      url: "/packages",
-      type: "POST",
-      datatype: "json",
-      data: package_info,
-      success: function(id_json) {
-        if(id_json.query_id != "NOEXIST") {
-          insertTable(id_json)
+      url: '/packages',
+      type: 'POST',
+      datatype: 'json',
+      data: packageInfo,
+      success: function (idJson) {
+        if (idJson.query_id !== 'NOEXIST') {
+          insertTable(idJson)
         }
       },
-      error: function(xhr, errmsg, err) {
-        console.log(xhr.status + ": " + xhr.responseText)
+      error: function (xhr, errmsg, err) {
+        console.log(xhr.status + ': ' + xhr.responseText)
       }
-    });
+    })
   }
 
-  function insertTable(id_json) {
-    var table = document.getElementById("packages-list");
-    var row = table.insertRow(1);
-    var cell0 = row.insertCell(0);
-    var cell1 = row.insertCell(1);
-    var cell2 = row.insertCell(2);
-    cell0.innerHTML = id_json.query_id;
-    cell1.innerHTML = id_json.blackcat_id + ": None at None";
-    cell2.innerHTML = id_json.chinese_id + ": None at None";
+  function insertTable (idJson) {
+    var table = document.getElementById("packages-list")
+    var row = table.insertRow(1)
+    var cell0 = row.insertCell(0)
+    var cell1 = row.insertCell(1)
+    var cell2 = row.insertCell(2)
+    cell0.innerHTML = idJson.query_id
+    cell1.innerHTML = idJson.blackcat_id + ': None at None'
+    cell2.innerHTML = idJson.chinese_id + ': None at None'
   }
 
   // for django csrf token
-  function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-      var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = jQuery.trim(cookies[i]);
-          if (cookie.substring(0, name.length + 1) == (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-            break;
-          }
+  function getCookie (name) {
+    var cookieValue = null
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';')
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i])
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break
         }
+      }
     }
-    return cookieValue;
+    return cookieValue
   }
-  var csrftoken = getCookie('csrftoken');
+  var csrftoken = getCookie('csrftoken')
 
-  function csrfSafeMethod(method) {
-      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  function csrfSafeMethod (method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method))
   }
 
   $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
+    beforeSend: function (xhr, settings) {
       if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        xhr.setRequestHeader('X-CSRFToken', csrftoken)
       }
     }
-  });
-});
+  })
+})
